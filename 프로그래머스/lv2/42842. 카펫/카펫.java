421fd47d803450d4f28554e4c3f974381d471c2b
Kim-Seong-Ho <81570533+kksshh0612@users.noreply.github.com>
2023-04-18 16:19:15 +0900

    [level 2] Title: 카펫, Time: 0.26 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {        //노랑의 가로+세로+가로+세로+4 = 갈색
+    
+    
+    
+   
+    
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};      //가로, 세로 중 큰거 먼저 
+        answer = new int[2];
+        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> ansArrayList = new ArrayList<>();
+        
+        for(int i = 1; i <= yellow; i++){
+            if(yellow % i == 0){
+                arr.add(i);
+                
+            }
+        }
+        //인덱스
+        int left = 0;
+        int right = arr.size() - 1;
+        
+        while(left <= right){
+            int leftNum = arr.get(left++);
+            int rightNum = arr.get(right--);
+
+            if((leftNum + rightNum) * 2 + 4 == brown){
+                ansArrayList.add(leftNum + 2);
+                ansArrayList.add(rightNum + 2);
+            }
+        }
+        Collections.sort(ansArrayList, Comparator.reverseOrder());
+        
+        answer[0] = ansArrayList.get(0);
+        answer[1] = ansArrayList.get(1);
+        
+        return answer;
+    }
+}

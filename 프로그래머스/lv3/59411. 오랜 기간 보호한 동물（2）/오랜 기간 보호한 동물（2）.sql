d977f444dc220f5b9e72fb1e6c478cfeeb15036f
Kim-Seong-Ho <81570533+kksshh0612@users.noreply.github.com>
2023-04-18 12:38:27 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT outs.ANIMAL_ID, outs.NAME
+from ANIMAL_INS as ins inner join ANIMAL_OUTS as outs
+    on ins.ANIMAL_ID = outs.ANIMAL_ID
+order by datediff(outs.DATETIME, ins.DATETIME) desc
+limit 2

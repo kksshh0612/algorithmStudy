6a9f4708e1d1fd1b72d987eaaae45e826ddd4459
Kim-Seong-Ho <81570533+kksshh0612@users.noreply.github.com>
2023-04-22 17:22:17 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,5 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 from ANIMAL_INS 
-where NAME = 'Lucy'or NAME = 'Ella' or NAME = 'Pickle' or NAME = 'Rogan' or NAME = 'Sabrina' or NAME = 'Mitty'
+where NAME in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+order by ANIMAL_ID

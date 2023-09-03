8a03f321301ab9883836a69931fd9b10d968e061
Kim-Seong-Ho <81570533+kksshh0612@users.noreply.github.com>
2023-09-03 12:28:23 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    
+    public int oneNum(int n){
+        
+        int one = 0;
+        
+        while(n > 0){
+            if(n % 2 == 1){
+                one++;
+            }
+            n /= 2;
+        }
+        
+        return one;
+    }
+    
+    public int solution(int n) {
+        int answer = 0;
+        int originOneNum = 0, currOneNum = -1;
+        
+        originOneNum = oneNum(n);
+        while(originOneNum != currOneNum){
+            currOneNum = oneNum(++n);    
+        }
+        
+        answer = n;
+        
+        return answer;
+    }
+}

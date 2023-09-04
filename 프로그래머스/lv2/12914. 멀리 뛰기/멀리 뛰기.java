bc20340c2b168639eb89ba94ed8b040d8d8d58dd
Kim-Seong-Ho <81570533+kksshh0612@users.noreply.github.com>
2023-09-04 21:21:40 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        
+        if(n >= 2){
+            dp[2] = 2; 
+        }
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 1] % 1234567 + dp[i - 2] % 1234567) % 1234567;
+        }
+        
+        answer = dp[n];
+        
+        return answer;
+    }
+}

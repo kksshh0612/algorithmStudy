ac7882bcb11c885874621db8c8eda7a81e1195d0
Seongho Kim <81570533+kksshh0612@users.noreply.github.com>
2024-09-28 16:14:40 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.96 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -0,0 +1,66 @@
+import java.util.*;
+// 유니온 파인드
+class Solution {
+    
+    public static int find(int[] parent, int n){
+        if(parent[n] == n) return n;
+        else return find(parent, parent[n]);
+    }
+    
+    public static void union(int[] parent, int n1, int n2){
+        // int n1 = find(parent, n1);
+        // int n2 = find(parent, n2);
+        
+        int min = Math.min(n1, n2);
+        
+        parent[n1] = min;
+        parent[n2] = min;
+    }
+    
+    public int solution(int[] cards) {
+        int answer = 1;
+        
+        int[] parent = new int[cards.length + 1];
+        
+        for(int i = 0; i < parent.length; i++){
+            parent[i] = i;
+        }
+        
+        for(int i = 0; i < cards.length; i++){
+            int n1 = find(parent, i + 1);
+            int n2 = find(parent, cards[i]);
+            
+            // System.out.println(i + 1 + " " + cards[i] + " / " + n1 + " " + n2);
+            
+            if(n1 != n2){
+                union(parent, n1, n2);
+            }
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 1; i < parent.length; i++){
+            int key = find(parent, i);
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        for(Integer key : map.keySet()){
+            list.add(map.get(key));
+        }
+        Collections.sort(list, Comparator.reverseOrder());
+        
+        // for(int i = 0; i < list.size(); i++){
+        //     System.out.println(list.get(i));
+        // }
+        
+        answer *= list.get(0);
+        if(list.size() == 1){
+            answer = 0;
+        }
+        else{
+            answer *= list.get(1);
+        }
+        
+        return answer;
+    }
+}

3a41470e89334acce242189b55a63f7e8c156678
Seongho Kim <81570533+kksshh0612@users.noreply.github.com>
2024-10-11 13:24:17 +0900

    [Silver III] Title: N과 M (6), Time: 100 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static int[] arr;
+    public static int n, m;
+    public static StringBuilder ans = new StringBuilder();
+
+    public static void dfs(int depth, int currIdx, int[] currArr){
+        if(depth == m){
+            for(int i = 0; i < m; i++){
+                ans.append(currArr[i]).append(" ");
+            }
+            ans.append("\n");
+        }
+        else{
+            for(int i = currIdx; i < n; i++){
+                currArr[depth] = arr[i];
+                dfs(depth + 1, i + 1, currArr);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, 0, new int[m]);
+
+        System.out.println(ans);
+    }
+}

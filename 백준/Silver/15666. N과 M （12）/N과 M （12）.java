bcb4e4887e1632cef7c399e55661935194e0ca10
Seongho Kim <81570533+kksshh0612@users.noreply.github.com>
2024-10-12 11:51:20 +0900

    [Silver II] Title: N과 M (12), Time: 120 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static int n, m;
+    public static int[] arr;
+    public static StringBuilder ans = new StringBuilder();
+
+    public static void dfs(int depth, int start, int[] currArr){
+        if(depth == m){
+            for(int num : currArr){
+                ans.append(num).append(" ");
+            }
+            ans.append("\n");
+        }
+        else{
+            int before = 0;
+            for(int i = start; i < n; i++){
+                if(arr[i] == before) continue;
+
+                before = arr[i];
+                currArr[depth] = arr[i];
+                dfs(depth + 1, i, currArr);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0, 0, new int[m]);
+
+        System.out.println(ans.toString());
+    }
+}
